64a133bfbe1c0c40c7077dd772f9f9068814cb13
Manan Doshi <manan.doshi@colorado.edu>
2024-02-10 05:13:38 -0700

    Moved everything inside useeffect

--- src/components/hooks/useScrollStatus.js
+++ src/components/hooks/useScrollStatus.js
@@ -3,26 +3,27 @@ import { useState, useEffect } from 'react'
 const useScrollStatus = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
 
-  const getDocHeight = () => {
-    return Math.max(
-      document.body.scrollHeight, document.documentElement.scrollHeight,
-      document.body.offsetHeight, document.documentElement.offsetHeight,
-      document.body.clientHeight, document.documentElement.clientHeight
-    );
-  }
-
-  const calculateScrollDistance = () => {
-    const scrollTop = window.pageYOffset; // how much the user has scrolled by
-    const winHeight = window.innerHeight;
-    const docHeight = getDocHeight();
-    const totalDocScrollLength = docHeight - winHeight;
-    const scrollValue = Math.floor(scrollTop / totalDocScrollLength * 100);
-
-    setScrollPosition(scrollValue);
-  }
 
 
   useEffect(() => {
+    const getDocHeight = () => {
+      return Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+      );
+    }
+
+    const calculateScrollDistance = () => {
+      const scrollTop = window.pageYOffset; // how much the user has scrolled by
+      const winHeight = window.innerHeight;
+      const docHeight = getDocHeight();
+      const totalDocScrollLength = docHeight - winHeight;
+      const scrollValue = Math.floor(scrollTop / totalDocScrollLength * 100);
+
+      setScrollPosition(scrollValue);
+    }
+
     const listenToScrollEvent = () => {
       document.addEventListener("scroll", () => {
         requestAnimationFrame(() => {
